f4fb642162d81fbb2866463730b9adf5c7013bf0
SofyaPim <sofanpim@mail.ru>
2023-04-25 13:14:14 +0300

    add classes

--- lessons/classes/script.js
+++ lessons/classes/script.js
@@ -3,4 +3,24 @@ class Rectangle {
       this.height = height;
       this.width = width;
     }
+    calcArea(){
+        return this.height * this.width;
+    }
   }
+  const squere = new Rectangle(12,12);
+  console.log(squere.calcArea());
+  
+
+  class ColoredRectangle extends Rectangle{
+    constructor(height, width, color, title) {
+       super(height, width);
+       this.color = color;
+       this.title = title;
+      }
+      showMyProps(){
+        console.log(`text  - ${this.title}, color  - ${this.color}`)
+      }
+
+  }
+  const div = new ColoredRectangle(30,45, 'green','me');
+  console.log(div.showMyProps());
